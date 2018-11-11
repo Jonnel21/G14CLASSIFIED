fcf1ebf7684813f3d17cf97bfd299a05cb2c979d
a-chen01 <albert.chen01@sjsu.edu>
2018-11-10 22:50:36 -0800

    Refactored files

---
+++ src/run.py
@@ -0,0 +1,18 @@
+import cocos
+from main_menu.hello_world import HelloWorld
+
+if __name__ == "__main__":
+    # director init takes the same arguments as pyglet.window
+    cocos.director.director.init()
+
+    # We create a new layer, an instance of HelloWorld
+    hello_layer = HelloWorld()
+
+    # A scene that contains the layer hello_layer
+    main_scene = cocos.scene.Scene(hello_layer)
+
+    # And now, start the application, starting with main_scene
+    cocos.director.director.run(main_scene)
+
+    #      director.run( cocos.scene.Scene( HelloWorld() ) )
+    # or you could have written, without so many comments:
